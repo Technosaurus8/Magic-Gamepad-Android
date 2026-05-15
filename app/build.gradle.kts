08045258562c13d1041b885f32165d9d44f324e0
Aman Muhammed <amanmuhammed515@gmail.com>
2026-05-15 11:21:25 +0530

    updated all the packages agp etc

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.technosaurus.MagicGamepad"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.technosaurus.MagicGamepad"
+        minSdk = 24
+        targetSdk = 37
+        versionCode = 16
+        versionName = "2.4"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.play.services.ads)
+    implementation(libs.java.websocket)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.activity)
+    implementation(project(":joystickView"))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ joystickView/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.zerokol.views.joystickView"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(libs.appcompat)
+    implementation(libs.ext.junit)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.espresso.core)
+}
