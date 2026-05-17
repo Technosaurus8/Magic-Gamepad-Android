6b8646de3f09676de40c2d42af63aa92a1592798
Aman Muhammed <amanmuhammed515@gmail.com>
2026-05-17 12:16:55 +0530

    fixed status bar visibility on android 14+

--- app/src/main/java/com/technosaurus/MagicGamepad/MainActivity.kt
+++ app/src/main/java/com/technosaurus/MagicGamepad/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT))
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
         setContent {
             MaterialTheme {
                 val navController = rememberNavController()
