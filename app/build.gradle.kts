cc689e15f858c440d9a2634f743bf159db80115b
Aman Muhammed <amanmuhammed515@gmail.com>
2026-08-04 13:24:47 +0530

    updated the version and uploading a new aab with the debug symbol for better crash analysis.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.technosaurus.MagicGamepad"
         minSdk = 23
         targetSdk = 37
-        versionCode = 22
-        versionName = "4.1"
+        versionCode = 23
+        versionName = "4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
