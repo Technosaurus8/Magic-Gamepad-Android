aa786c83b28c2eab835a2295a23254b87400e66f
Aman Muhammed <amanmuhammed515@gmail.com>
2026-08-04 13:11:20 +0530

    included the debug symbol in the aab itself

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,9 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
     compileOptions {
